72ecc6c5efb8b8f79c7b8eb36cfc34fbe3ace5af
Thomas Markiewicz <tomek@librem.one>
2020-05-05 01:03:46 -0500

    Formatted code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use druid::widget::prelude::*;
 use druid::widget::{Align, Flex, Label, Padding};
 use druid::{AppLauncher, Data, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
-use log::{info};
+use log::info;
 
 #[derive(Clone, Data, Lens)]
 struct Reps {
@@ -11,7 +11,6 @@ struct Reps {
 
 impl Reps {
     fn toggle(&mut self) {
-        
         self.reps = match self.reps {
             None => Some(self.target_reps),
             Some(current) => {
@@ -53,27 +52,29 @@ fn build_ui() -> impl Widget<()> {
 fn build_ui() -> impl Widget<Reps> {
     Padding::new(
         8.0,
-        Flex::column()
-            .with_child(
-                // TODO: see calc example `op_button_label` for an example of a label with a custom background (round for example)
-                // TODO: see invalidations example `CircleView` for drawing a circle background/button
-                Label::new(|reps: &Reps, _env: &_| String::from(format!("{}", reps.reps.unwrap_or(reps.target_reps))))
+        Flex::column().with_child(
+            // TODO: see calc example `op_button_label` for an example of a label with a custom background (round for example)
+            // TODO: see invalidations example `CircleView` for drawing a circle background/button
+            Label::new(|reps: &Reps, _env: &_| {
+                String::from(format!("{}", reps.reps.unwrap_or(reps.target_reps)))
+            })
+            .with_text_size(48.0)
+            .center()
+            .on_click(|_ctx, reps, _env| {
+                reps.toggle();
+            }),
+            /*
+            Label::new(|reps: &Option<u8>, _env: &_| String::from(format!("{}", reps.unwrap_or(0))))
                 .with_text_size(48.0)
                 .center()
+                .lens(Reps::reps)
                 .on_click( |_ctx, reps, _env| { reps.toggle();}),
-                /*
-                Label::new(|reps: &Option<u8>, _env: &_| String::from(format!("{}", reps.unwrap_or(0))))
-                    .with_text_size(48.0)
-                    .center()
-                    .lens(Reps::reps)
-                    .on_click( |_ctx, reps, _env| { reps.toggle();}),
-                */
-            )
+            */
+        ),
     )
 }
 
 fn main() -> Result<(), PlatformError> {
-
     let reps = Reps {
         target_reps: 8,
         reps: None,
