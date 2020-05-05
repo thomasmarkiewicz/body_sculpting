7c6b24760f2035614f950ec3efa34f45312a6c14
Thomas Markiewicz <tomek@librem.one>
2020-05-05 00:56:15 -0500

    Added tappable label that simulates the round reps button for setting the rep count

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
+use druid::widget::prelude::*;
 use druid::widget::{Align, Flex, Label, Padding};
-use druid::{AppLauncher, PlatformError, Widget, WindowDesc};
+use druid::{AppLauncher, Data, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
+use log::{info};
 
+#[derive(Clone, Data, Lens)]
+struct Reps {
+    target_reps: u8,
+    reps: Option<u8>,
+}
+
+impl Reps {
+    fn toggle(&mut self) {
+        
+        self.reps = match self.reps {
+            None => Some(self.target_reps),
+            Some(current) => {
+                if current > 0 {
+                    Some(current - 1)
+                } else {
+                    None
+                }
+            }
+        };
+
+        info!("reps: {:?}", self.reps);
+    }
+}
+
+/*
 fn build_ui() -> impl Widget<()> {
     Padding::new(
         8.0,
@@ -21,8 +48,40 @@ fn build_ui() -> impl Widget<()> {
             ),
     )
 }
+*/
+
+fn build_ui() -> impl Widget<Reps> {
+    Padding::new(
+        8.0,
+        Flex::column()
+            .with_child(
+                Label::new(|reps: &Reps, _env: &_| String::from(format!("{}", reps.reps.unwrap_or(reps.target_reps))))
+                .with_text_size(48.0)
+                .center()
+                .on_click( |_ctx, reps, _env| { reps.toggle();}),
+                /*
+                Label::new(|reps: &Option<u8>, _env: &_| String::from(format!("{}", reps.unwrap_or(0))))
+                    .with_text_size(48.0)
+                    .center()
+                    .lens(Reps::reps)
+                    .on_click( |_ctx, reps, _env| { reps.toggle();}),
+                */
+            )
+    )
+}
 
 fn main() -> Result<(), PlatformError> {
-    AppLauncher::with_window(WindowDesc::new(build_ui).show_titlebar(false)).launch(())?;
+
+    let reps = Reps {
+        target_reps: 8,
+        reps: None,
+    };
+
+    let main_window = WindowDesc::new(build_ui).show_titlebar(false);
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(reps)?;
+
     Ok(())
 }
