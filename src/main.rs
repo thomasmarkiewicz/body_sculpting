77b7ef37c32072d279c738def1bf3c0a07c65aaa
Thomas Markiewicz <tomek@librem.one>
2020-05-06 18:53:05 -0500

    Turned the tappable label into a widget composition that looks like a red round button with padding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use druid::kurbo::{Circle, Point, Size};
 use druid::widget::prelude::*;
-use druid::widget::{Align, Flex, Label, Padding};
-use druid::{AppLauncher, Data, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::widget::{Align, Flex, Label, Padding, Painter, SizedBox};
+use druid::{theme, AppLauncher, Color, Data, Lens, PlatformError, Widget, WidgetExt, WindowDesc};
 use log::info;
 
 #[derive(Clone, Data, Lens)]
@@ -49,28 +50,51 @@ fn build_ui() -> impl Widget<()> {
 }
 */
 
+fn rep_button() -> impl Widget<Reps> {
+    let painter = Painter::new(|ctx, _, env| {
+        let bounds = ctx.size().to_rect();
+
+        //ctx.fill(bounds, &Color::rgb8(0xff, 0x52, 0x52));
+
+        let radius = (bounds.x1 - bounds.x0) / 2.0;
+        let center = Point::from((bounds.x0 + radius, bounds.y0 + radius));
+
+        ctx.fill(Circle::new(center, radius), &Color::rgb8(0xff, 0x52, 0x52));
+
+        if ctx.is_hot() {
+            ctx.stroke(Circle::new(center, radius - 1.0), &Color::WHITE, 1.0);
+        }
+
+        if ctx.is_active() {
+            ctx.fill(Circle::new(center, radius), &env.get(theme::PRIMARY_LIGHT));
+        }
+    });
+
+    let label = Label::new(|reps: &Reps, _env: &_| {
+        String::from(format!("{}", reps.reps.unwrap_or(reps.target_reps)))
+    })
+    .with_text_size(36.0)
+    .center()
+    .on_click(|_ctx, reps, _env| {
+        reps.toggle();
+    });
+
+    let sized_box = SizedBox::new(label)
+        .width(64.)
+        .height(64.)
+        .background(painter);
+
+    Padding::new(8.0, sized_box)
+}
+
 fn build_ui() -> impl Widget<Reps> {
-    Padding::new(
-        8.0,
-        Flex::column().with_child(
-            // TODO: see calc example `op_button_label` for an example of a label with a custom background (round for example)
-            // TODO: see invalidations example `CircleView` for drawing a circle background/button
-            Label::new(|reps: &Reps, _env: &_| {
-                String::from(format!("{}", reps.reps.unwrap_or(reps.target_reps)))
-            })
-            .with_text_size(48.0)
-            .center()
-            .on_click(|_ctx, reps, _env| {
-                reps.toggle();
-            }),
-            /*
-            Label::new(|reps: &Option<u8>, _env: &_| String::from(format!("{}", reps.unwrap_or(0))))
-                .with_text_size(48.0)
-                .center()
-                .lens(Reps::reps)
-                .on_click( |_ctx, reps, _env| { reps.toggle();}),
-            */
-        ),
+    Padding::new(8.0, 
+        Flex::row().must_fill_main_axis(true)
+            .with_flex_child(rep_button(), 1.0)
+            .with_flex_child(rep_button(), 1.0)
+            .with_flex_child(rep_button(), 1.0)
+            .with_flex_child(rep_button(), 1.0)
+            .with_flex_child(rep_button(), 1.0)
     )
 }
 
@@ -80,7 +104,9 @@ fn main() -> Result<(), PlatformError> {
         reps: None,
     };
 
-    let main_window = WindowDesc::new(build_ui).show_titlebar(false);
+    let main_window = WindowDesc::new(build_ui)
+        .show_titlebar(false)
+        .window_size(Size::new(720.0, 1320.0));
 
     AppLauncher::with_window(main_window)
         .use_simple_logger()
